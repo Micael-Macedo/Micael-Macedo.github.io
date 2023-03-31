d0b6df97fab1228f5af01032b00db04468b9addb
Micael Macedo <micaelm2009@hotmail.com>
2023-03-31 19:42:59 -0300

    feat: add animations

--- js/script.js
+++ js/script.js
@@ -27,3 +27,15 @@ function ExibirMenu(){
         MobileMenu.style.display = "none";
     }
 }
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) =>observer.observe(el));
