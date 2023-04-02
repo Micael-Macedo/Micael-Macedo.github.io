74b5197f0dd26488d61c175e73989d0e4590f7b4
Micael Macedo <micaelm2009@hotmail.com>
2023-04-01 23:56:27 -0300

    feat: add new mobile nav and add scss

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-var TechSkill = document.getElementById('TechSkills').getElementsByClassName('TechSkillsIcons');
-var TechSkillDescription = document.getElementById('TechSkillsDescriptions').getElementsByClassName('TechSkillsDescriptions');
-var statusSkill = 0;
-function ShowDescription(ChosenTechSkill) {
-    for (i = 0; i < TechSkill.length; i++) {
-
-        if (i == ChosenTechSkill) {
-            if(statusSkill == 0){
-                TechSkillDescription[i].style.display = "block";
-                statusSkill = 1;
-            }else if(statusSkill == 1){
-                TechSkillDescription[i].style.display = "none";
-                statusSkill = 0;
-            }
-        } else
-            if (i != ChosenTechSkill) {
-                TechSkillDescription[i].style.display = "none";
-            }
-    }
-}
 function ExibirMenu(){
     MobileMenu = document.getElementById("MobileMenu");
     if(MobileMenu.style.display == "none"){
